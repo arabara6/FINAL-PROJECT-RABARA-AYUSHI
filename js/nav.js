26c69bc63cc8c7d17cd4250bfc61e279b9d71475
arabara <arabara@iu.edu>
2022-05-26 20:56:25 -0500

    first commit

---
+++ js/nav.js
@@ -0,0 +1,15 @@
+/*NAVIGATION*/
+
+function openNav() {
+    document.querySelector('#mySidenav').style.width = "250px"; 
+    document.querySelector('.all-over-bkg').classList.add('is-visible');
+  }
+  
+  function closeNav() {
+    document.querySelector('#mySidenav').style.width = "0"; 
+    document.querySelector('.all-over-bkg').classList.remove('is-visible');
+  }
+  
+  document.querySelector('.openbtn').addEventListener('click', openNav);
+  document.querySelector('.closebtn').addEventListener('click', closeNav);
+  
